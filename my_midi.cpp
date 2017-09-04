ff79ed9798e7d47cc1aae26ff15eda067e5bd69c
Andy Smith <andyrsmith@gmail.com>
2017-09-04 23:08:16 +0100

    UNstable. Multi-channel.

--- my_midi.cpp
+++ my_midi.cpp
@@ -18,6 +18,7 @@
 #include <sys/timerfd.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <vector>
 
 static void error(const char *format, ...) {
   va_list ap;
@@ -84,6 +85,19 @@ static void list_device(snd_ctl_t *ctl, int card, int device) {
   }
 }
 
+struct my_chan {
+  std::string port;
+  snd_rawmidi_t *input;
+  snd_rawmidi_t **inputp;
+
+public:
+  my_chan(const std::string s) : port(s) {
+    std::cout << "Woot " << s << std::endl;
+    inputp = &input;
+    std::cout << "ptr=" << input << " *ptr=" << inputp << std::endl;
+  }
+};
+
 int main(int argc, char *argv[]) {
   // device_list();
   // list_card_devices(1);
@@ -101,89 +115,91 @@ int main(int argc, char *argv[]) {
   auto x2 = snd_ctl_rawmidi_next_device(ctl, &device);
   std::cout << "x2 is " << std::to_string(x2) << std::endl;
   std::cout << "device is " << device << std::endl;
-  list_device(ctl, x, device);
+  //list_device(ctl, x, device);
 
-  snd_rawmidi_t *input, **inputp;
-  snd_rawmidi_t *output, **outputp;
+  // snd_rawmidi_t *nput, **inputp;
+  // snd_rawmidi_t *output, **outputp;
 
-  outputp = &output;
-  inputp = &input;
-  std::cout << "Input is " << input << std::endl;
+  std::vector<my_chan> ports = { {"hw:1,0,0"} }; //, {"hw:1,0,1"}, {"hw:1,0,2"} };
+  //  std::cout << "Input is " << input << std::endl;
 
-  std::string port{"hw:0,0"};
-
-  if ((err = snd_rawmidi_open(inputp, outputp, argv[1], SND_RAWMIDI_NONBLOCK)) <
-      0) {
-    error("cannot open port \"%s\": %s", port.c_str(), snd_strerror(err));
-  }
-  std::cout << "Input is " << input << std::endl;
-
-  snd_rawmidi_read(input, NULL, 0); /* trigger reading */
-
-  if (inputp) {
-    int read = 0;
-    int npfds;
-    struct pollfd *pfds;
-
-    npfds = 1 + snd_rawmidi_poll_descriptors_count(input);
-    pfds = (pollfd *)alloca(npfds * sizeof(struct pollfd));
-    pfds[0].fd = -1;
-
-    snd_rawmidi_poll_descriptors(input, &pfds[1], npfds - 1);
-
-    // signal(SIGINT, sig_handler);
-
-    std::cout << "Main loop" << std::endl;
-
-    for (;;) {
-      //std::cout << "Poll" << std::endl;
-      unsigned char buf[256];
-      int i, length;
-      unsigned short revents;
-
-      err = poll(pfds, npfds, -1);
-      if (err < 0) {
-        error("poll failed: %s", strerror(errno));
-        break;
-      }
-
-      err = snd_rawmidi_poll_descriptors_revents(input, &pfds[1], npfds - 1,
-                                                 &revents);
-      if (err < 0) {
-        error("cannot get poll events: %s", snd_strerror(errno));
-        break;
-      }
-      if (revents & (POLLERR | POLLHUP))
-        break;
-      if (!(revents & POLLIN)) {
-        if (pfds[0].revents & POLLIN)
-          breai
-        continue;
-      }
-
-      err = snd_rawmidi_read(input, buf, sizeof(buf));
-      if (err == -EAGAIN)
-        continue;
-      if (err < 0) {
-        error("cannot read from port \"%s\": %s", port.c_str(), snd_strerror(err));
-        break;
-      }
-      length = 0;
-      for (i = 0; i < err; ++i)
-        // if ((buf[i] != MIDI_CMD_COMMON_CLOCK &&
-        //      buf[i] != MIDI_CMD_COMMON_SENSING) ||
-        //     (buf[i] == MIDI_CMD_COMMON_CLOCK && !ignore_clock) ||
-        //     (buf[i] == MIDI_CMD_COMMON_SENSING && !ignore_active_sensing))
-          buf[length++] = buf[i];
-      if (length == 0)
-        continue;
-      read += length;
-
-      for (i = 0; i < length; ++i) {
-        std::cout << std::hex << buf[i] << " ";
-        //print_byte(buf[i]);
-      };
-      std::cout << std::endl;
+  for ( auto& chan : ports) {
+    std::cout << "port is " << chan.port << std::endl;
+    std::cout << "input is " << chan.input << " ptr=" << chan.inputp << std::endl;
+    if ((err = snd_rawmidi_open(chan.inputp, nullptr, chan.port.c_str(), SND_RAWMIDI_NONBLOCK)) <
+        0) {
+      error("cannot open port \"%s\": %s", chan.port.c_str(), snd_strerror(err));
     }
+    std::cout << "input is " << chan.input << " ptr=" << chan.inputp << std::endl;
+    snd_rawmidi_read(chan.input, NULL, 0); /* trigger reading */
+  }
+  //std::cout << "Input is " << input << std::endl;
+
+
+  int read = 0;
+  int npfds;
+  struct pollfd *pfds;
+
+  npfds = 1 + snd_rawmidi_poll_descriptors_count(ports[0].input);
+  pfds = (pollfd *)alloca(npfds * sizeof(struct pollfd));
+  pfds[0].fd = -1;
+
+  std::cout << "npfds is " << std::endl;
+
+  snd_rawmidi_poll_descriptors(ports[0].input, &pfds[1], npfds - 1);
+
+  // signal(SIGINT, sig_handler);
+
+  std::cout << "Main loop" << std::endl;
+
+  for (;;) {
+    // std::cout << "Poll" << std::endl;
+    unsigned char buf[256];
+    int i, length;
+    unsigned short revents;
+
+    err = poll(pfds, npfds, -1);
+    if (err < 0) {
+      error("poll failed: %s", strerror(errno));
+      break;
+    }
+
+    err = snd_rawmidi_poll_descriptors_revents(ports[0].input, &pfds[1], npfds - 1,
+                                               &revents);
+    if (err < 0) {
+      error("cannot get poll events: %s", snd_strerror(errno));
+      break;
+    }
+    if (revents & (POLLERR | POLLHUP))
+      break;
+    if (!(revents & POLLIN)) {
+      if (pfds[0].revents & POLLIN)
+        continue;
+    }
+
+    err = snd_rawmidi_read(ports[0].input, buf, sizeof(buf));
+    if (err == -EAGAIN)
+      continue;
+    if (err < 0) {
+      error("cannot read from port \"%s\": %s", "foo",
+            snd_strerror(err));
+      break;
+    }
+    length = 0;
+    for (i = 0; i < err; ++i)
+      // if ((buf[i] != MIDI_CMD_COMMON_CLOCK &&
+      //      buf[i] != MIDI_CMD_COMMON_SENSING) ||
+      //     (buf[i] == MIDI_CMD_COMMON_CLOCK && !ignore_clock) ||
+      //     (buf[i] == MIDI_CMD_COMMON_SENSING && !ignore_active_sensing))
+      buf[length++] = buf[i];
+    if (length == 0)
+      continue;
+    read += length;
+
+    for (i = 0; i < length; ++i) {
+      std::cout << std::hex << buf[i] << " ";
+      // print_byte(buf[i]);
+    };
+    std::cout << std::endl;
   }
 };
