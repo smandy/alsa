8e9a8e7c6f17a4df7ec32f67841a2d949baca37d
Andy Smith <andyrsmith@gmail.com>
2020-02-27 00:07:58 +0000

    Incremental.

--- gen.cpp
+++ gen.cpp
@@ -1,11 +1,13 @@
 #include "SDL2/SDL_audio.h"
 #include <SDL2/SDL.h>
+#include <cstdint>
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
 
 double angle;
 uint64_t counter;
+double Hz = 6000;
 
 // Do all the init stuff
 void init_sdl(void) {
@@ -13,35 +15,34 @@ void init_sdl(void) {
     exit(-1);
   atexit(SDL_Quit);
 }
-double Hz = 6000;
 
-// Creates the sine wave. To supress ripple, the wave runs continuously by using
-// an incemental counter
-void Callback(void *userdata, uint8_t *stream, int len) {
+const float m1 = 0.90;
+float mult = m1;
+;
+
+void callback(void *userdata, uint8_t *stream, int len) {
   double pi = 3.1415;
-  // uint8_t *waveptr;
-  // double Hz=220;
-  Hz *= 0.99;
-  if (Hz < 400)
-    Hz = 6000;
-  // double L = 1024;
-  double A = 20000;
+  Hz *= mult;
+  if (Hz < 100)
+    mult = 1 / m1;
+  else if (Hz > 6000)
+    mult = m1;
+
+  double A = INT16_MAX;
   double SR = 44100;
   double F = 2 * pi * Hz / SR;
-  int16_t *s2 = (int16_t *)stream;
+  auto s2 = reinterpret_cast<int16_t *>(stream);
   for (int z = 0; z < (len / 2); z++) {
     counter++;
     angle += F;
-    s2[z] = (uint16_t) A * sin(angle);
-    if (counter % 16284 == 0) {
+    s2[z] = (int16_t)(A * sin(angle));
+    if ((counter & ((1 << 14) - 1)) == 0) {
       std::cout << counter << " hz=" << Hz << " len=" << len << std::endl;
-    };
+    }
   }
 }
 
 void play(void) {
-  // sound_buffer = new uint8_t[1024];
-  // sound_len= 1024;
   SDL_AudioSpec spec;
   spec.freq = 44100;
   spec.format = AUDIO_S16SYS;
@@ -51,7 +52,7 @@ void play(void) {
   spec.padding = 0;
   spec.size = 0;
   spec.userdata = 0;
-  spec.callback = Callback;
+  spec.callback = callback;
   if (SDL_OpenAudio(&spec, NULL) < 0) {
     printf("Kann audio nicht ?ffnen: %s\n", SDL_GetError());
     exit(-1);
@@ -60,11 +61,11 @@ void play(void) {
 }
 
 int main(int argc, char *argv[]) {
-  std::cout << "STartup" << std::endl;
+  std::cout << "Startup" << std::endl;
   init_sdl();
   play();
-  std::cout << "delay" << std::endl;
+  std::cout << "Delay" << std::endl;
   SDL_Delay(20000);
-  std::cout << "return" << std::endl;
+  std::cout << "Return" << std::endl;
   return 0;
 }
